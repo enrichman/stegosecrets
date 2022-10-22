376f747cbd24782444c84cee467a8e39a80b5393
garugaru <garuglieritommaso.dev@gmail.com>
2022-10-22 16:50:53 +0200

    added unit tests for pkg packages

---
+++ pkg/file/file_test.go
@@ -0,0 +1,39 @@
+package file
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path"
+	"testing"
+)
+
+func Test_WriteFileChecksum(t *testing.T) {
+	tmpDir := t.TempDir()
+	original := path.Join(tmpDir, "file")
+
+	err := os.WriteFile(original, []byte("content"), 0600)
+	require.NoError(t, err)
+
+	err = WriteFileChecksum(original)
+	require.NoError(t, err)
+
+	checksum, err := os.ReadFile(fmt.Sprintf("%s.checksum", original))
+	require.NoError(t, err)
+
+	expectedChecksum := []byte("ed7002b439e9ac845f22357d822bac1444730fbdb6016d3ec9432297b9ec9f73\tfile")
+	require.Equal(t, expectedChecksum, checksum)
+}
+
+func Test_WriteKey(t *testing.T) {
+	tmpDir := t.TempDir()
+	keyFile := path.Join(tmpDir, "file")
+
+	expectedKey := []byte("test")
+	err := WriteKey(expectedKey, keyFile)
+	require.NoError(t, err)
+
+	key, err := ReadKey(keyFile + ".key")
+	require.NoError(t, err)
+	require.Equal(t, expectedKey, key)
+}
---
+++ pkg/stego/aes_test.go
@@ -0,0 +1,65 @@
+package stego
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_EncryptDecrypt(t *testing.T) {
+	key, err := GenerateMasterKey()
+	require.NoError(t, err)
+
+	message := []byte("test message")
+	encrypted, err := Encrypt(key, message)
+	require.NoError(t, err)
+
+	decrypted, err := Decrypt(key, encrypted)
+	require.NoError(t, err)
+	require.Equal(t, message, decrypted)
+}
+
+func Test_EncryptDecryptEmptyMessage(t *testing.T) {
+	key, err := GenerateMasterKey()
+	require.NoError(t, err)
+
+	message := []byte{}
+	encrypted, err := Encrypt(key, message)
+	require.NoError(t, err)
+
+	decrypted, err := Decrypt(key, encrypted)
+	require.NoError(t, err)
+	require.Equal(t, message, decrypted)
+}
+
+func Benchmark_Encrypt(b *testing.B) {
+	key, err := GenerateMasterKey()
+	if err != nil {
+		b.Fatal(err)
+	}
+	message := []byte("test message")
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_, err := Encrypt(key, message)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func Benchmark_Decrypt(b *testing.B) {
+	key, err := GenerateMasterKey()
+	if err != nil {
+		b.Fatal(err)
+	}
+	encr, err := Encrypt(key, []byte("test message"))
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_, err := Decrypt(key, encr)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
