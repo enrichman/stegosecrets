6a412b182615ee456ae6be1632249d48c48fc844
Enrico Candino <enrico.candino@gmail.com>
2022-11-06 01:28:30 +0100

    added encrypt tests

--- internal/encrypt/encrypt_test.go
+++ internal/encrypt/encrypt_test.go
@@ -1,12 +1,16 @@
 package encrypt_test
 
 import (
+	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/enrichman/stegosecrets/internal/encrypt"
+	"github.com/enrichman/stegosecrets/internal/log"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -116,4 +120,37 @@ func TestNewEncrypter_WithImagesDir(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, absoluteImagesDir, encrypter.ImagesDir)
 	})
+}
+
+func TestEncrypt(t *testing.T) {
+	tmpDir, err := os.MkdirTemp(os.TempDir(), "out-*")
+	assert.Nil(t, err)
+	assert.DirExists(t, tmpDir)
+
+	encrypter, err := encrypt.NewEncrypter(
+		encrypt.WithParts(5),
+		encrypt.WithThreshold(2),
+		encrypt.WithOutputDir(tmpDir),
+		encrypt.WithImagesDir("../../test/assets/p5t3"),
+		encrypt.WithLogger(log.NewSimpleLogger(io.Discard, log.None)),
+	)
+	assert.Nil(t, err)
+
+	err = encrypter.Encrypt(strings.NewReader("hello world!"), "secret")
+	assert.Nil(t, err)
+
+	assert.DirExists(t, tmpDir)
+	assert.FileExists(t, tmpDir+"/secret.enc")
+	assert.FileExists(t, tmpDir+"/secret.enc.key")
+	assert.FileExists(t, tmpDir+"/secret.checksum")
+	assert.FileExists(t, tmpDir+"/secret.enc.checksum")
+
+	for i := 1; i <= 5; i++ {
+		assert.FileExists(t, fmt.Sprintf("%s/%03d.jpg", tmpDir, i))
+		assert.FileExists(t, fmt.Sprintf("%s/%03d.jpg.checksum", tmpDir, i))
+		assert.FileExists(t, fmt.Sprintf("%s/%03d.key", tmpDir, i))
+	}
+
+	err = os.RemoveAll(tmpDir)
+	assert.Nil(t, err)
 }
